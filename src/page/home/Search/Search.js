396e0e29f3ebf89312a346bbb6868bb411064ae8
md masud rana <mdmasudranae7@gmail.com>
2023-03-05 00:17:19 +0600

    search data get

--- src/page/home/Search/Search.js
+++ src/page/home/Search/Search.js
@@ -1,5 +1,5 @@
 import { useQueries, useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Search = () => {
   const { data: categorys = [] } = useQuery({
@@ -8,82 +8,79 @@ const Search = () => {
       fetch(" http://localhost:5000/categories").then((res) => res.json()),
   });
 
-
   const { data: locations = [] } = useQuery({
     queryKey: ["locations"],
     queryFn: () =>
       fetch(" http://localhost:5000/locations").then((res) => res.json()),
   });
 
+  const [homes, setHome] = useState([]);
 
-     
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const from = event.target;
+    const location = from.location.value;
+    const category = from.category.value;
 
-
-
-
-
-
-  const handleSearch = event => {
-      event.preventDefault()
-      const from = event.target;
-      const location = from.location.value;
-      const category = from.category.value;
-      console.log("event", category, location);
-      fetch(`http://localhost:5000/search/location?${location}`).then(res => res.json()).then(data => console.log(data)).catch(err => console.log(err))
-
-  }
+    fetch(
+      `http://localhost:5000/search?location=${location}&category=${category}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setHome(data);
+      })
+      .catch((err) => console.log(err));
+  };
+  console.log(homes);
 
   return (
     <div className="container mx-auto ">
       <form onSubmit={handleSearch}>
-      <div className="grid lg:grid-cols-3 sm:grid-cols-1 mt-10 justify-items-center ">
-        <div className=" rounded-lg flex flex-col gap-2 mb-4  ">
-          <label htmlFor="email" className="text-xl font-medium">
-            Select Category
-          </label>
-          <select
-            required
-            name="category"
-            className="select w-full max-w-xs  select-bordered"
-            
-          >
-            <option disabled selected>
-              Select category
-            </option>
-            {categorys.map((category) => (
-              <option key={category._id} value={category?.category}>
-                {category?.category}
+        <div className="grid lg:grid-cols-3 sm:grid-cols-1 mt-10 justify-items-center ">
+          <div className=" rounded-lg flex flex-col gap-2 mb-4  ">
+            <label htmlFor="email" className="text-xl font-medium">
+              Select Category
+            </label>
+            <select
+              required
+              name="category"
+              className="select w-full max-w-xs  select-bordered">
+              <option disabled selected>
+                Select category
               </option>
-            ))}
-          </select>
-        </div>
+              {categorys.map((category) => (
+                <option key={category._id} value={category?.category}>
+                  {category?.category}
+                </option>
+              ))}
+            </select>
+          </div>
 
-        <div className=" rounded-lg flex flex-col gap-2 mb-4  ">
-          <label htmlFor="email" className="text-xl font-medium">
-            Select Category
-          </label>
-          <select
-            required
-            name="location"
-            className="select w-full max-w-xs  select-bordered"
-          >
-            <option disabled selected>
-              Select Location
-            </option>
-            {locations.map((location) => (
-              <option key={location._id} value={location?.location}>
-                {location?.location}
+          <div className=" rounded-lg flex flex-col gap-2 mb-4  ">
+            <label htmlFor="email" className="text-xl font-medium">
+              Select Category
+            </label>
+            <select
+              required
+              name="location"
+              className="select w-full max-w-xs  select-bordered">
+              <option disabled selected>
+                Select Location
               </option>
-            ))}
-          </select>
-        </div>
+              {locations.map((location) => (
+                <option key={location._id} value={location?.location}>
+                  {location?.location}
+                </option>
+              ))}
+            </select>
+          </div>
 
-        <div className="mt-9">
-          <button type="search" className="btn btn-primary">
-            Search
-          </button>
+          <div className="mt-9">
+            <button type="search" className="btn btn-primary">
+              Search
+            </button>
+          </div>
         </div>
-      </div>
       </form>
     </div>
   );
