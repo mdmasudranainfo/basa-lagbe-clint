87a741a6985129be531cbf10d1af1c7a7b8ec202
mdmasudranainfo <mdmasude7@gmail.com>
2023-03-30 23:47:52 +0600

    asd

--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -44,7 +44,7 @@ function Nav() {
   );
 
   return (
-    <div className="shadow-lg bg-black ">
+    <div className="shadow-lg bg-black sticky top-0 z-50">
       <div className="navbar container mx-auto">
         <div className="navbar-start">
           <div className="dropdown">
